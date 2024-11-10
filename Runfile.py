19d6ee75182c0fc3d1801c212cda5fbdfac1fa7f
Samuel Zhang <simpzan@gmail.com>
2024-11-10 21:16:43 +0800

    cleanup Runfile.py

--- Runfile.py
+++ Runfile.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-import run
-from run import sh, sh_out, sh_async, sh_out_async, log
+from run import run_main, sh, sh_out, sh_async, sh_out_async, log
 
 def log_test():
     import time
@@ -14,24 +13,8 @@ def log_test():
 
 def ssh_test():
     sh('ssh jx')
-
-def shell(func):
-    def wrapper_shell(*args, **kwargs):
-        return sh(func.__doc__)
-    return wrapper_shell
-
-@shell
-def kernel_info(): r'''
-uname -a
-'''
-
-def sh_test():        
-    sh('''
-uname -r
-pwd
-       ''')
     
-def test():
+def shell_test():
     p_async_stream = sh_async('sleep 2; uptime') # async, stream
     p_async_pipe = sh_out_async("sleep 1; date") # async, pipe
 
@@ -45,4 +28,4 @@ def test():
     print('p_async_pipe', out.strip())
     p_async_stream.wait()
 
-if __name__ == "__main__": run.run_main(__file__)
+if __name__ == "__main__": run_main(__file__)
