daab724b4c0f9b11082e7fc697fbe462bfda4d12
Your Name <you@example.com>
2025-02-22 08:22:56 +0000

    vmc.py: fix cloned hdd path issue

--- vmc.py
+++ vmc.py
@@ -142,7 +142,7 @@ def _get_hdd(vm):
 def fork(base, vm):
     base_hdd = _get_hdd(base)
     import os.path
-    new_hdd = os.path.dirname(base_hdd) + vm + '.qcow2'
+    new_hdd = f'{os.path.dirname(base_hdd)}/{vm}.qcow2'
     cmd = f'''qemu-img create -f qcow2 -F qcow2 -b {base_hdd} "{new_hdd}" &&
         virt-clone --original "{base}" --name "{vm}" --file "{new_hdd}" --preserve-data &&
         virt-sysprep -d {vm} --operation machine-id'''
